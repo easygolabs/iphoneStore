6877e857349ab296497c949e518f30948ede236f
sleizo <sleizo@ukr.net>
2023-11-13 13:11:42 +0200

    fix: Use @Repository annotation

--- src/main/java/com/example/iphoneStore/repository/GoodsRepository.java
+++ src/main/java/com/example/iphoneStore/repository/GoodsRepository.java
@@ -2,9 +2,11 @@ package com.example.iphoneStore.repository;
 
 import com.example.iphoneStore.model.Goods;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GoodsRepository extends CrudRepository<Goods, Long> {
     Optional<Goods> findByName(String name);
 }
--- src/main/java/com/example/iphoneStore/repository/OrderRepository.java
+++ src/main/java/com/example/iphoneStore/repository/OrderRepository.java
@@ -3,11 +3,13 @@ package com.example.iphoneStore.repository;
 import com.example.iphoneStore.model.Order;
 import com.example.iphoneStore.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Optional<Order> findByUser(User user);
 
